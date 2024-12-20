3b2b2447a0af85a201e14ad7f7e89609706e5252
Johnny Marnell <johnnymarnell@gmail.com>
2024-12-20 12:44:40 -0500

    testing cdn

---
+++ budibase.js
@@ -0,0 +1,8 @@
+/*
+
+WiP:
+determine column order by .header-cell .name contents
+then .grid-body .row .cell:nth-child(n)
+override last of the style="flex: 0 0 42px;" of these with custom sizes
+
+*/
